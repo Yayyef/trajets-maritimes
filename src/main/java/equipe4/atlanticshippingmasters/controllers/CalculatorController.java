951689efb6ea02469f5603d45e5309b07af55107
Xavier Chassang <chassang.xavier@gmail.com>
2022-09-02 10:16:22 +0200

    Ajout fichiers calculator

--- src/main/java/equipe4/atlanticshippingmasters/controllers/CalculatorController.java
+++ src/main/java/equipe4/atlanticshippingmasters/controllers/CalculatorController.java
@@ -1,5 +1,25 @@
 package equipe4.atlanticshippingmasters.controllers;
 
-public class CalculatorController {
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import equipe4.atlanticshippingmasters.model.Port;
+import equipe4.atlanticshippingmasters.service.PortService;
+
+@Controller
+public class CalculatorController {
+	
+	@Autowired
+	private PortService ps;
+	
+	@GetMapping("/calculator")
+	public String getCalculator(Model model) {
+		
+		Iterable<Port> portList = ps.getAllPorts();
+		model.addAttribute("ports", portList);
+		return "calculator";
+	}
 }
