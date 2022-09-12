93d3c309343e75b4943b5c668eadfb05fb5e9d83
anhara <anhara.susoma@gmail.com>
2022-09-12 12:31:07 +0200

    update of page journey and added details step

--- src/main/java/equipe4/atlanticshippingmasters/controllers/JourneyController.java
+++ src/main/java/equipe4/atlanticshippingmasters/controllers/JourneyController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import equipe4.atlanticshippingmasters.model.Journey;
 import equipe4.atlanticshippingmasters.service.JourneyService;
 
 @Controller
@@ -19,5 +22,13 @@ public class JourneyController {
 			return "journeys";
 			
 		}
+		
+		@GetMapping("/details/{id}")
+		public String JourneyView(@PathVariable Integer id ,Model model) {
+			model.addAttribute("journey",journeyService.getJourney(id).orElse(null));
+			 
+			return "details";
+		}
+		
 	
 }
