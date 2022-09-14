5b529f949d516198d71e7e70e7e80dd9bdf451cd
Xavier Chassang <chassang.xavier@gmail.com>
2022-09-14 14:32:02 +0200

    Ajout pagination. Changements style.

--- src/main/java/equipe4/atlanticshippingmasters/service/JourneyService.java
+++ src/main/java/equipe4/atlanticshippingmasters/service/JourneyService.java
@@ -1,6 +1,11 @@
 package equipe4.atlanticshippingmasters.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,21 +17,38 @@ import equipe4.atlanticshippingmasters.model.JourneyRepository;
 public class JourneyService {
 	@Autowired
 	private JourneyRepository pr;
-	
+
 	// Chercher et retourner un Journey unique avec son id
 	public Optional<Journey> getJourney(final Integer id) {
 		return pr.findById(id);
 	}
-	
-	// Chercher et retourner une collection contenant tous les objets de type Journey de ma bdd
+
+	// Chercher et retourner une collection contenant tous les objets de type
+	// Journey de ma bdd
 	public Iterable<Journey> getAllJourneys() {
 		return pr.findAll();
 	}
-	
+
+	// Méthode pour récupérer une page de journeys
+	public List<Journey> getPage(int pageNumber, int itemsPerPage) {
+		// On converti l'itérable récupéré en liste pour avoir accès à get()
+		List<Journey> allJourneys = StreamSupport.stream(pr.findAll().spliterator(), false).collect(Collectors.toList());
+		List<Journey> pageList = new ArrayList<>();
+		
+		for (int i = (pageNumber*itemsPerPage) - itemsPerPage; i < pageNumber*itemsPerPage; i++) {
+			if (i >= allJourneys.size()) break;
+			pageList.add(allJourneys.get(i));
+		}
+				
+		return pageList;
+	}
+
 	public void deleteJourney(final int id) {
 		pr.deleteById(id);
 	}
+
 	public void insertJourney(Journey Journey) {
 		pr.save(Journey);
 	}
+
 }
